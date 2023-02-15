fc51a064121c1a72474ae9acc5061f3459830f98
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-02-15 16:39:51 -0600

    able to view and delete individual notes now

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const dbNotes = require('./db/db.json')
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
@@ -15,7 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(express.static('public'));
 
 
@@ -24,13 +24,15 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+
 // GET Route for note page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-const readFromFile = util.promisify(fs.readFile);
 
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
 
 /**
  *  Function to write data to the JSON file given a destination and some content
@@ -67,17 +69,18 @@ app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+
 // POST Route for a new note
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
-  const { title, text, note_id } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
 
     readAndAppend(newNote, './db/db.json');
@@ -86,6 +89,22 @@ app.post('/api/notes', (req, res) => {
     res.error('Error in adding Note');
   }
 });
+
+
+// delete a single note
+app.delete('/api/notes/:id', (req, res) => {
+  let db = JSON.parse(fs.readFileSync('db/db.json'))
+  let deleteNote = db.filter(note => note.id != req.params.id);
+  fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
+  res.json(deleteNote);
+  console.info(`${req.method} request received to delete note`);
+})
+
+
+// wildcard 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 
 app.listen(PORT, () =>
